b358db3a67b06853898e789937ec07cd862dd7d3
Beatriz Amaral <beatriz@webplanet.com.br>
2024-02-26 12:46:40 +0000

    feat(TicketG3stor): Refatorar código

--- design/defaulttheme/tpl/lhchat/chat_tabs/information_rows/ticket_g3stor.tpl.php
+++ design/defaulttheme/tpl/lhchat/chat_tabs/information_rows/ticket_g3stor.tpl.php
@@ -1,13 +1,17 @@
 <tr>
     <td colspan="2">
-        <h6 class="fw-bold"><i class="material-icons">account_box</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','G3stor')?></h6>
+        <h6 class="fw-bold"><i class="material-icons">account_box</i>
+            <?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat', 'G3stor') ?>
+        </h6>
         <div class="row">
             <div class="col-6 pb-1">
-                <div class="text-muted" id="create-ticket-<?php echo $chat->id?>" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Criar chamado')?>">
-                <?php?>
-                <span class="material-icons action-image" onclick='lhc.revealModal({"iframe" : true, "title" : <?php echo json_encode(htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Criar Chamado')), JSON_HEX_QUOT)?>, height: "600", "url":WWW_DIR_JAVASCRIPT+"chat/ticketg3stor"})'
-                 title="Criar Chamado">chamado</span>Criar Chamado
-                    <?php?>    
+                <div class="text-muted" id="create-ticket-<?php echo $chat->id ?>"
+                    title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat', 'Criar chamado') ?>">
+                    <?  ?>
+                    <span class="material-icons action-image"
+                        onclick='lhc.revealModal({"iframe" : true, "title" : <?php echo json_encode(htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat', 'Criar Chamado')), JSON_HEX_QUOT) ?>, height: "600", "url":WWW_DIR_JAVASCRIPT+"chat/ticketg3stor"})'
+                        title="Criar Chamado">chamado</span>Criar Chamado
+                    <?  ?>
                 </div>
             </div>
         </div>
--- design/defaulttheme/tpl/lhchat/ticketg3stor.tpl.php
+++ design/defaulttheme/tpl/lhchat/ticketg3stor.tpl.php
@@ -1,7 +1,5 @@
 <style>
-
-<?php include 'design/defaulttheme/css/g3stor.css'; ?>
-
+    <?php include 'design/defaulttheme/css/g3stor.css'; ?>
 </style>
 
 
@@ -14,10 +12,10 @@
 
     <label for="atendente">Atendente:</label>
     <input type="text" id="atendente" name="atendente" required><br><br>
-    
+
     <label for="descricao_problema">Descrição do Problema:</label>
     <textarea id="descricao_problema" name="descricao_problema" rows="4" required></textarea><br><br>
-    
+
     <label for="setor">Setor:</label>
     <select id="setor" name="setor" required>
         <option value="">Selecione o setor</option>
@@ -28,21 +26,21 @@
     </select><br><br>
 
     <fieldset>
-    <legend>Vincular conversa ao chamado?</legend>
-    <div style="display: flex; padding: 5px;">
-        <input type="checkbox" id="sim" name="sim" checked />
-        <label for="sim">Sim</label>
-        <input type="checkbox" id="nao" name="nao" />
-        <label for="nao">Não</label>
-    </div>
+        <legend>Vincular conversa ao chamado?</legend>
+        <div style="display: flex; padding: 5px;">
+            <input type="checkbox" id="sim" name="sim" checked />
+            <label for="sim">Sim</label>
+            <input type="checkbox" id="nao" name="nao" />
+            <label for="nao">Não</label>
+        </div>
     </fieldset>
 
     <input type="submit" value="Enviar">
 </form>
 
 <script>
-function onCreateTicket() {
-    alert("ENVIADO");
-    return false;
-}
+    function onCreateTicket() {
+        alert("ENVIADO");
+        return false;
+    }
 </script>
