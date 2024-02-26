c4df692c88657e7bb8caa66d58bd93b517fcbbe1
Beatriz Amaral <beatriz@webplanet.com.br>
2024-02-26 12:47:38 +0000

    feat(InfosTicketsG3stor): Novo metodo de comuniação com a api)

--- design/defaulttheme/tpl/lhchat/deleteTicket.tpl.php
+++
@@ -1,8 +0,0 @@
-<script>
-    lhc.revealModal({
-        "iframe": true,
-        "title": "<?php echo htmlspecialchars_decode(erTranslationClassLhTranslation::getInstance()->getTranslation('chat/adminchat','Criar Chamado')); ?>",
-        "height": "600",
-        "url": "<?php echo WWW_DIR_JAVASCRIPT; ?>chat/ticketg3stor"
-    });
-</script>
--- design/defaulttheme/tpl/lhchat/infosticketg3stor.tpl.php
+++ design/defaulttheme/tpl/lhchat/infosticketg3stor.tpl.php
@@ -1,42 +1,51 @@
 <style>
-
-<?php include 'design/defaulttheme/css/g3stor.css'; ?>
-
+    <?php include 'design/defaulttheme/css/g3stor.css'; ?>
 </style>
 
 <div class="col-12 pt-1" style="padding: 15px">
-<button style="margin: 15px;" onclick=''>Encerrar o chamado</button>
-    <!-- <button style="margin: 15px;" onclick='var_dump(rLhcoreClassDesign::designtpl('lhchat/deleteTicket.tpl.php'));?>'>Encerrar o chamado</button> -->
-    <h1>Ficha nº <?php echo $infos['listaMVSUP']['SUP_COD']; ?></h1>
-    
-    
+    <div style="display: flex; justify-content: space-between;">
+        <h1>Ficha nº
+            <?php echo $infos['listaMVSUP']['SUP_COD']; ?>
+        </h1>
+        <button style="margin: 15px;" onclick="closeTicket()">Encerrar o chamado</button>
+    </div>
+
+
 
     <h2>Informações Gerais</h2>
     <div style="display: flex; flex-wrap: wrap;">
         <div style="flex: 1; padding-right: 20px;">
-            <h3>Cadastrado em:</h3><?php echo $infos['listaMVSUP']['SUP_DAT']; ?>
+            <h3>Cadastrado em:</h3>
+            <?php echo $infos['listaMVSUP']['SUP_DAT']; ?>
         </div>
         <div style="flex: 1; padding-right: 20px;">
-            <h3>Por:</h3> <?php echo $infos['listaMVSUP']['FUN_RED']; ?> 
+            <h3>Por:</h3>
+            <?php echo $infos['listaMVSUP']['FUN_RED']; ?>
         </div>
     </div>
 </div>
 
 <div class="col-12 pt-1" style="padding: 15px">
-<h4 style="margin-right: 20px;">Assunto: </br><input type="text" id="assunto" name="assunto" value="<?php echo $infos['listaMVSUP']['SUP_ASS']; ?>"></h4>
-<div style="display: flex;">
-<h4 style="margin-right: 20px;">Cliente:</br> <input type="text" id="cliente" name="cliente" value="<?php echo $infos['listaMVSUP']['CLI_NOM']; ?>"></h4>
-    <h4 style="margin-right: 20px;">Área:</br> <input type="text" id="area" name="area" value="<?php echo $infos['listaMVSUP']['ARE_NOM']; ?>"></h4>
-    <h4 style="margin-right: 20px;">Contato:</br> <input type="text" id="contato" name="contato" value="<?php echo $infos['listaMVSUP']['SUP_CON']; ?>"></h4>
-</div>
-<div style="display: flex;">
-    <h4 style="margin-right: 20px;">Categoria: </br><input type="text" id="categoria" name="categoria" value="<?php echo $infos['listaMVSUP']['CAT_NOM']; ?>"></h4>
-    <h4 style="margin-right: 20px;">Funcionário:</br> <input type="text" id="funcionario" name="funcionario" value="<?php echo $infos['listaMVSUP']['FUN_RED']; ?>"></h4>
-    <h4 style="margin-right: 20px;">Situação: </br><input type="text" id="situacao" name="situacao" value="<?php echo $infos['listaMVSUP']['SIT_NOM']; ?>"></h4>
-</div>
+    <h4 style="margin-right: 20px;">Assunto: </br><input type="text" id="assunto" name="assunto"
+            value="<?php echo $infos['listaMVSUP']['SUP_ASS']; ?>"></h4>
+    <div style="display: flex;">
+        <h4 style="margin-right: 20px;">Cliente:</br> <input type="text" id="cliente" name="cliente"
+                value="<?php echo $infos['listaMVSUP']['CLI_NOM']; ?>"></h4>
+        <h4 style="margin-right: 20px;">Área:</br> <input type="text" id="area" name="area"
+                value="<?php echo $infos['listaMVSUP']['ARE_NOM']; ?>"></h4>
+        <h4 style="margin-right: 20px;">Contato:</br> <input type="text" id="contato" name="contato"
+                value="<?php echo $infos['listaMVSUP']['SUP_CON']; ?>"></h4>
+    </div>
+    <div style="display: flex;">
+        <h4 style="margin-right: 20px;">Categoria: </br><input type="text" id="categoria" name="categoria"
+                value="<?php echo $infos['listaMVSUP']['CAT_NOM']; ?>"></h4>
+        <h4 style="margin-right: 20px;">Funcionário:</br> <input type="text" id="funcionario" name="funcionario"
+                value="<?php echo $infos['listaMVSUP']['FUN_RED']; ?>"></h4>
+        <h4 style="margin-right: 20px;">Situação: </br><input type="text" id="situacao" name="situacao"
+                value="<?php echo $infos['listaMVSUP']['SIT_NOM']; ?>"></h4>
+    </div>
 </div>
 
-<button style="margin: 15px;" onclick="adicionarLinha()">Adicionar Nova Observação</button>
 <div style="padding: 15px">
     <table class="table table-hover table-sm" id="comments" border="1">
         <thead>
@@ -49,27 +58,87 @@
         <tbody>
             <?php foreach ($obs['listaMVSUP2'] as $comments): ?>
                 <tr>
-                    <td><?php echo $comments['DAT_CAD']; ?></td>
-                    <td><?php echo $comments['USE_SIG']; ?></td>
-                    <td><?php echo $comments['SUP2_OBS']; ?></td>
+                    <td>
+                        <?php echo $comments['DAT_CAD']; ?>
+                    </td>
+                    <td>
+                        <?php echo $comments['USE_SIG']; ?>
+                    </td>
+                    <td>
+                        <?php echo $comments['SUP2_OBS']; ?>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
     </table>
 </div>
+<div style="padding: 15px">
+    <textarea id="comentario" name="comentario" placeholder="Digite um comentário" rows="4"></textarea></br>
+    <button style="margin: 15px;" onclick="addComment()">Adicionar Nova Observação</button>
+</div>
 
 <script>
-function adicionarLinha() {
-    var table = document.getElementById("comments").getElementsByTagName('tbody')[0];
-    var newLine = table.insertRow();
+    function addComment() {
+        var apiUrl = 'https://demo4705385.mockable.io/comments';
+        var token = 'dGVzdHNlcnZlcjp0ZXN0c2VydmVy';
 
-    var columnData = newLine.insertCell(0);
-    var columnEmployee = newLine.insertCell(1);
-    var columnComments = newLine.insertCell(2);
+        var data = {
+            ticketId: <?php echo $infos['listaMVSUP']['SUP_COD']; ?>,
+            comment: comentario.input,
+            author: "LHC Web"
 
-    columnData.innerHTML = '19/02/2024';
-    columnEmployee.innerHTML = 'Novo Funcionário';
-    columnComments.innerHTML = 'Nova Observação';
-}
+        };
+
+        var options = {
+            method: 'POST', // trocar para Post ou delete quando a api ficar pronta 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify(data)
+        };
+
+        fetch(apiUrl, options)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao enviar comentário');
+                }
+                alert('Comentário Enviado!');
+                location.reload();
+            })
+            .catch(error => {
+                console.error('Erro ao enviar comentário: ', error);
+            });
+    }
+
+    function closeTicket() {
+        var apiUrl = 'https://demo4705385.mockable.io/ticket';
+        var token = 'dGVzdHNlcnZlcjp0ZXN0c2VydmVy';
+
+        var data = {
+            ticketId: <?php echo $infos['listaMVSUP']['SUP_COD']; ?>
+        };
+
+        var options = {
+            method: 'DELETE', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            // body: JSON.stringify(data) 
+        };
+
+        fetch(apiUrl, options)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao fechar o ticket');
+                }
+                alert('Ticket fechado com sucesso');
+                location.reload();
+            })
+            .catch(error => {
+                console.error('Erro ao fechar o ticket:', error);
+            });
+    }
 
 </script>
